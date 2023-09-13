56ac2f838215e877ccdb96e7597a6f19965c1120
Max Zabelenkov <maxx.zabelenkov@gmail.com>
2023-09-13 22:29:47 +0300

    done

--- js/swiper.js
+++ js/swiper.js
@@ -1,7 +1,26 @@
-const swiper = new Swiper('.swiper', {
-    autoplay: {
-        delay: 1500,
-    },
-    slidesPerView:3,
-    loop: true,
-});
+"use strict";
+
+(function () {
+    'use strict';
+    var breakpoint = window.matchMedia('(max-width:53.125rem)');
+    var swiper;
+    var breakpointChecker = function breakpointChecker() {
+        if (breakpoint.matches === true) {
+            if (swiper !== undefined) swiper.destroy(true, true);
+            return;
+        } else if (breakpoint.matches === false) {
+            return enableSwiper();
+        }
+    };
+    var enableSwiper = function enableSwiper() {
+        swiper = new Swiper('.swiper', {
+            autoplay: {
+                delay: 1500,
+            },
+            loop: true,
+            slidesPerView: 3,
+        });
+    };
+    breakpoint.addListener(breakpointChecker);
+    breakpointChecker();
+})();
